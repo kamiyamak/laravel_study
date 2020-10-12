5408f3cecc3b24de475084f1f73aabf5214ad8a2
kamiyamak <kamiyamak@istyle.co.jp>
2020-10-12 18:18:58 +0900

    tutorial-4

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/test', function () {
+    return view('welcome');
+});
+
+Route::get('/sample', 'SampleController@index');
+Route::get('/select', 'SampleController@select');
+Route::get('/select_many', 'SampleController@selectMany');
+Route::get('/insert', 'SampleController@insert');
+Route::get('/delete', 'SampleController@delete');
+Route::get('/update', 'SampleController@update');
