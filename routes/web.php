9bb571cb17a87013eac624f2db38f526666f83b6
kamiyamak <kamiyamak@istyle.co.jp>
2020-12-29 15:56:58 +0900

    tutorial5-Done

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,13 @@ Route::get('/insert', 'SampleController@insert');
 Route::get('/delete', 'SampleController@delete');
 
 Route::get('/update', 'SampleController@update');
+
+Route::get('/form/index', 'SampleFormController@index');
+
+Route::get('/form/show/{id}', 'SampleFormController@show');
+
+Route::post('/form/store', 'SampleFormController@store');
+
+Route::post('/form/delete', 'SampleFormController@delete');
+
+Route::post('/form/update', 'SampleFormController@update');
